c6c43360d630cb4d7bca0b2ced87afb3eb06efb6
24110257ak <24110257@student.hcmute.edu.vn>
2026-08-27 11:17:15 +0700

    Delete template directory

--- template/src/main/java/com/koha/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.koha.dao;
-
-import com.koha.model.UserModel;
-
-public interface UserDao {
-    UserModel findByUsername(String username);
-}
--- template/src/main/java/com/koha/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.koha.service;
-
-import com.koha.model.UserModel;
-
-public interface UserService {
-    UserModel login(String username, String password);
-}
